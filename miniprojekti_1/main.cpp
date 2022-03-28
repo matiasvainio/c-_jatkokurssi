307e5a8c48a16e8e80c129df5885833b94e3f500
Matias Vainio <matias.s.vainio@gmail.com>
2022-03-28 10:44:27 +0300

    added testing functionality to main

--- miniprojekti_1/main.cpp
+++ miniprojekti_1/main.cpp
@@ -24,17 +24,19 @@ template <typename T>
 class log_ptr {
    private:
     reference_counter* rc;
-    T* ptr;
+    T* data;
 
+    // Write line to log file.
     void write_line(string s) {
         auto timestamp = get_timestamp();
 
         ofstream o;
-        o.open("/home/matias/dev/c/c++_jatkokurssi/miniprojekti_1/temp.txt", std::ios_base::app);
-        o << timestamp << " " << s << " " << ptr << "\n";
+        o.open("/home/matias/dev/c/cpp-jatkokurssi/miniprojekti_1/ptr_log.txt", std::ios_base::app);
+        o << timestamp << " " << s << " " << data << "\n";
         o.close();
     }
 
+    // Get current time.
     auto get_timestamp() {
         auto time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
         auto timestamp = std::ctime(&time);
@@ -44,7 +46,7 @@ class log_ptr {
 
    public:
     log_ptr(T* value) {
-        ptr = value;
+        data = value;
 
         rc = new reference_counter();
         rc->add_ref();
@@ -54,7 +56,7 @@ class log_ptr {
 
     ~log_ptr() {
         if (rc->release() == 0) {
-            delete ptr;
+            delete data;
             delete rc;
         }
 
@@ -63,31 +65,65 @@ class log_ptr {
 
     T* operator->() {
         write_line("operator->");
-        return ptr;
+        return data;
     }
 
     T& operator*() {
         write_line("operator*");
-        return *ptr;
+        return *data;
     }
 
     // Remove copy constructor and copy assignment operator.
     // log_ptr(const log_ptr&) = delete;
     // log_ptr& operator=(const log_ptr&) = delete;
 
-    log_ptr(const log_ptr<T>& log_ptr) : ptr(log_ptr.ptr), rc(log_ptr.rc) {
+    // Copy constructor.
+    log_ptr(const log_ptr<T>& log_ptr) : data(log_ptr.data), rc(log_ptr.rc) {
+        write_line("kopiokonstruktori");
         rc->add_ref();
     }
 
+    // Assignment operator.
+    log_ptr<T>& operator=(const log_ptr<T>& log_ptr) {
+        if (this != &log_ptr) {
+            if (rc->release() == 0) {
+                delete data;
+                delete rc;
+                write_line("olio tuhottu sijoitusoperaatiosta");
+            }
 
+            data = log_ptr.data;
+            rc = log_ptr.rc;
+            rc->add_ref();
+            write_line("sijoitusoperaatio");
+        }
+
+        return *this;
+    }
 };
 
 class foobar {
+   private:
+    string s;
+
    public:
-    string i = "foobar";
+    foobar(string s) : s(s) {}
+    string get_s() { return s; }
 };
 
 int main() {
-    log_ptr<foobar> ptr(new foobar);
-    *ptr;
+    log_ptr<foobar> p(new foobar("foobar"));
+    p->get_s();
+
+    {
+        log_ptr<foobar> q = p;
+        *q;
+
+        log_ptr<foobar> f(new foobar("bizbaz"));
+        // The above object gets removed when new object gets assigned.
+        f = q;
+        cout << f->get_s() << endl;
+    }
+    
+    cout << p->get_s() << endl;
 }
