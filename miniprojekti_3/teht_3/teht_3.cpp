0415cd9611fb96f36ab89cec36191de8829d3d41
Matias Vainio <matias@DESKTOP-70SR9KO.localdomain>
2022-04-20 00:34:11 +0300

    miniprojekti 3 done

--- miniprojekti_3/teht_3/teht_3.cpp
+++ miniprojekti_3/teht_3/teht_3.cpp
@@ -5,21 +5,22 @@ using namespace std;
 class Big_Data {
    private:
     int* buffer;
+    size_t size{0};
 
    public:
-    Big_Data(int i) {
-        buffer = (int*)malloc(i);
-    }
+    Big_Data(int i) : buffer((int*)malloc(i)), size(i){}
 
-    Big_Data(const Big_Data& big_data) {
-        buffer = (int*)malloc(sizeof(big_data.buffer));
-        *buffer = *big_data.buffer;
+    Big_Data(const Big_Data& other) {
+        buffer = (int*)malloc(size);
+        copy(other.buffer, other.buffer + sizeof(other.buffer), buffer);
     }
 
     ~Big_Data() { free(buffer); }
 
     Big_Data& operator=(const Big_Data& other) {
-        *buffer = *other.buffer;
+        free(buffer);
+        buffer = (int*)malloc(size);
+        copy(other.buffer, other.buffer + sizeof(other.buffer), buffer);
         return *this;
     }
 };
--- miniprojekti_3/teht_4/teht_4.cpp
+++ miniprojekti_3/teht_4/teht_4.cpp
@@ -5,34 +5,36 @@ using namespace std;
 class Big_Data {
    private:
     int* buffer;
+    size_t size{0};
 
    public:
-    Big_Data(int i) : buffer((int*)malloc(i)) {}
+    Big_Data(int i) : buffer((int*)malloc(i)), size(i) {}
 
-    Big_Data(const Big_Data& big_data) {
-        buffer = (int*)malloc(sizeof(big_data.buffer));
-        *buffer = *big_data.buffer;
+    Big_Data(const Big_Data& other) {
+        buffer = (int*)malloc(size);
+        copy(other.buffer, other.buffer + sizeof(other.buffer), buffer);
     }
 
     ~Big_Data() { free(buffer); }
 
-    int* get_buffer() {
-        return buffer;
-    }
-
-    void set_value(int i) {
-        *buffer = i;
-    }
-
     Big_Data& operator=(const Big_Data& other) {
-        *buffer = *other.buffer;
+        free(buffer);
+        buffer = (int*)malloc(size);
+        copy(other.buffer, other.buffer + sizeof(other.buffer), buffer);
         return *this;
     }
 
-    Big_Data(Big_Data&& other) : buffer(move(other.buffer)) {}
+    Big_Data(Big_Data&& other)
+        : size(other.size), buffer(other.buffer) {
+            other.buffer = nullptr;
+            other.size = 0;
+        }
 
-    Big_Data& operator=(Big_Data&& other) {
-        buffer = move(other.buffer);
+    auto& operator=(Big_Data&& other) {
+        buffer = other.buffer;
+        size = other.size;
+        other.buffer = nullptr;
+        other.size = 0;
         return *this;
     }
 };
@@ -42,7 +44,5 @@ int main() {
     Big_Data b(1024);
     b = Big_Data(1024);
     Big_Data c(std::move(a));
-    cout << c.get_buffer() << '\n';
-    cout << a.get_buffer() << '\n';
     return 0;
 }
