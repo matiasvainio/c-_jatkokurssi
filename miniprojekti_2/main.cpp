85cf5b3dd1435ed469af4153ba9d3d9fea750ad1
Matias Vainio <matias@DESKTOP-70SR9KO.localdomain>
2022-04-19 23:54:44 +0300

    before pull miniprojekti3

---
+++ miniprojekti_2/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+constexpr auto COLS = 300, ROWS = 300;
+
+double map[COLS][ROWS];
+
+double influence_map[COLS][ROWS];
+
+inline double distance() {
+    
+}
+
+int main() {
+    k
+    return 0;
+}
